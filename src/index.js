ef150afcb0b52e457ebf79e484ecc159555bc162
nmeek44 <nmeek44@gmail.com>
2021-04-12 14:16:25 -0400

    part 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+const breedUrl = 'https://dog.ceo/api/breeds/list/all' 
+document.addEventListener("DOMContentLoaded", function () {
+    let dogList = document.querySelector("#dog-breeds")
+    // fetch(imgUrl)
+    //     .then(res => res.json())
+    //     .then((data) => {
+    //         console.log(data.message)
+    //         data.message.forEach(image => {
+    //             let listItem = document.createElement("li")
+    //             let imageElement = document.createElement("img")
+    //             listItem.append(imageElement)
+    //             imageElement.src = image
+    //             dogList.append(listItem)
+    //             console.log(listItem)
+    //         })
+    //     })
+        fetch(breedUrl)
+            .then(res => res.json())
+            .then((data) => {
+                console.log(data.message.african)
+            } )
+})
