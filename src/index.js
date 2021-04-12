27884c3e0464ba832ea6b47350d4e34957ad6ba9
nmeek44 <nmeek44@gmail.com>
2021-04-12 16:33:12 -0400

    part 2

--- src/index.js
+++ src/index.js
@@ -19,9 +19,12 @@ document.addEventListener("DOMContentLoaded", function () {
         .then((data) => {
             let dogKeys = Object.keys(data.message)
             dogKeys.forEach((breed) => {
-                console.log(data.message[breed])
                 let newItem = document.createElement('li')
                 newItem.innerText = breed
+                newItem.addEventListener("click",function (event) {
+                    newItem.style.color = "blue"
+                 
+                })
                 if (data.message[breed] !== []) {
                     let newSubList = document.createElement('ul')
                     data.message[breed].forEach((subBreed) => {
@@ -29,9 +32,63 @@ document.addEventListener("DOMContentLoaded", function () {
                         newSubItem.innerText = subBreed
                         newSubList.append(newSubItem)
                         newItem.append(newSubList)
+                        newSubItem.addEventListener("click",function (event) {
+                            newSubItem.style.color = "blue"
+                        
+                        })
                     })
                 }
                 dogList.append(newItem)
             })
         } )
+        document.querySelector("#breed-dropdown").onchange = function (letter) {
+            dogList.innerText = ""
+            let dropDown = document.querySelector("#breed-dropdown").value
+            fetch(breedUrl)
+                .then(res => res.json())
+                .then((data) => {
+                    let dogKeys = Object.keys(data.message)
+                    if (dropDown === "a") {
+                        dogKeys = dogKeys.filter(function(dog) {
+                            return dog[0] === "a"
+                        })
+                    } else if (dropDown === "b") {
+                        dogKeys = dogKeys.filter(function(dog) {
+                            return dog[0] === "b"
+                        })
+                    } else if (dropDown === "c") {
+                        dogKeys = dogKeys.filter(function(dog) {
+                            return dog[0] === "c"
+                        })
+                    } else if (dropDown === "d") {
+                        dogKeys = dogKeys.filter(function(dog) {
+                            return dog[0] === "d"
+                        })
+                    }
+                    dogKeys.forEach((breed) => {
+                        let newItem = document.createElement('li')
+                        newItem.innerText = breed
+                        newItem.addEventListener("click",function (event) {
+                            newItem.style.color = "blue"
+                         
+                        })
+                        if (data.message[breed] !== []) {
+                            let newSubList = document.createElement('ul')
+                            data.message[breed].forEach((subBreed) => {
+                                let newSubItem = document.createElement('li')
+                                newSubItem.innerText = subBreed
+                                newSubList.append(newSubItem)
+                                newItem.append(newSubList)
+                                newSubItem.addEventListener("click",function (event) {
+                                    newSubItem.style.color = "blue"
+                                
+                                })
+                            })
+                        }
+                        dogList.append(newItem)
+                    })
+                } )
+            console.log()
+        }
+        
 })
