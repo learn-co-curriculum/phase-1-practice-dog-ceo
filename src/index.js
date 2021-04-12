85e857daadd8b18caf1a0302d54e5a789af7d1b8
Yosef <shmuli5770@gmail.com>
2021-04-12 17:09:24 -0400

    factored out forEach

--- src/index.js
+++ src/index.js
@@ -14,32 +14,14 @@ document.addEventListener("DOMContentLoaded", function () {
                 imageContainer.append(imageElement)
             })
         })
+
     fetch(breedUrl)
         .then(res => res.json())
         .then((data) => {
             let dogKeys = Object.keys(data.message)
-            dogKeys.forEach((breed) => {
-                let newItem = document.createElement('li')
-                newItem.innerText = breed
-                newItem.addEventListener("click",function () {
-                    newItem.style.color = "blue"
-                })
-                if (data.message[breed] !== []) {
-                    let newSubList = document.createElement('ul')
-                    data.message[breed].forEach((subBreed) => {
-                        let newSubItem = document.createElement('li')
-                        newSubItem.innerText = subBreed
-                        newSubList.append(newSubItem)
-                        newItem.append(newSubList)
-                        newSubItem.addEventListener("click",function (e) {
-                            newSubItem.style.color = "blue"
-                            e.stopPropagation()
-                        })
-                    })
-                }
-                dogList.append(newItem)
-            })
+            population(dogKeys,data)
         } )
+
         document.querySelector("#breed-dropdown").onchange = function (letter) {
             dogList.innerText = ""
             let dropDown = document.querySelector("#breed-dropdown").value
@@ -49,29 +31,31 @@ document.addEventListener("DOMContentLoaded", function () {
                     let dogKeys = Object.keys(data.message).filter(function(dog){
                         return dog[0] === dropDown
                     })
-                    dogKeys.forEach((breed) => {
-                        let newItem = document.createElement('li')
-                        newItem.innerText = breed
-                        newItem.addEventListener("click",function () {
-                            newItem.style.color = "blue"
-                        })
-                        if (data.message[breed] !== []) {
-                            let newSubList = document.createElement('ul')
-                            data.message[breed].forEach((subBreed) => {
-                                let newSubItem = document.createElement('li')
-                                newSubItem.innerText = subBreed
-                                newSubList.append(newSubItem)
-                                newItem.append(newSubList)
-                                newSubItem.addEventListener("click",function (e) {
-                                    newSubItem.style.color = "blue"
-                                    e.stopPropagation()
-                                })
-                            })
-                        }
-                        dogList.append(newItem)
-                    })
+                    population(dogKeys,data)
                 } )
-            console.log()
         }
-        
+    
+    function population(array, masterObject) {
+        array.forEach((breed) => {
+            let newItem = document.createElement('li')
+            newItem.innerText = breed
+            newItem.addEventListener("click",function () {
+                newItem.style.color = "blue"
+            })
+            if (masterObject.message[breed] !== []) {
+                let newSubList = document.createElement('ul')
+                masterObject.message[breed].forEach((subBreed) => {
+                    let newSubItem = document.createElement('li')
+                    newSubItem.innerText = subBreed
+                    newSubList.append(newSubItem)
+                    newItem.append(newSubList)
+                    newSubItem.addEventListener("click",function (e) {
+                        newSubItem.style.color = "blue"
+                        e.stopPropagation()
+                    })
+                })
+            }
+            dogList.append(newItem)
+        })
+    }
 })
