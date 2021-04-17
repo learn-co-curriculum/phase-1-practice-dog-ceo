bac01c9f5150831887c30fb37c61b96d500ae66a
Hpollard003 <hpollard003@gmail.com>
2021-04-17 17:22:03 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 //console.log('%c HI', 'color: firebrick')
-// document.addEventListener('DOMContentLoaded', () => {
-//     ceo()
-// })
+document.addEventListener('DOMContentLoaded', () => {
+    ceo()
+})
 
-// function ceo() {
-//     const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
-//     fetch(imgUrl)
-//     .then(resp => resp.json())
-//     .then(json => renderImages(json))
+function ceo() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => renderImages(json))
    
-// }
+}
 
-// function renderImages(images) {
-//     images.message.forEach(renderImage);
-// }
+function renderImages(images) {
+    images.message.forEach(renderImage);
+}
 
-// function renderImage(urlImg){
-//     const loc = document.getElementById('dog-image-container')
-//     //console.log(urlImg)
-//     const img = document.createElement('img')
-//     //console.dir(img)
-//     img.className = "dog-image"
-//     img.height = 300
-//     img.src = urlImg
-//     loc.appendChild(img)
+function renderImage(urlImg){
+    const loc = document.getElementById('dog-image-container')
+    //console.log(urlImg)
+    const img = document.createElement('img')
+    //console.dir(img)
+    img.className = "dog-image"
+    img.height = 300
+    img.src = urlImg
+    loc.appendChild(img)
     
-// }
+}
 
 function breeds() {
     const breedUrl = 'https://dog.ceo/api/breeds/list/all' 
