227a5ec8d42220979ee6cfbdf108b44492a24b6e
Jack Pena <penajackc@gmail.com>
2022-01-19 10:01:50 -0700

    first task done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+fetch(imgUrl).then(res => res.json()).then(data => addDogToDOM(data))
+
+function addDogToDOM(data){
+    let dogSpot = document.querySelector('#dog-image-container')
+    data.message.forEach(dog => {
+        let newDogElement = document.createElement('img')
+        newDogElement.setAttribute('src', dog)
+        dogSpot.append(newDogElement)
+    })
+}
